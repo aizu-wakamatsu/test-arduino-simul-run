2abb87bcf54a9a1b89c950523c4addcc492f8fc0
tari <aizuwakamatsu@outlook.jp>
2024-05-08 23:58:47 +0900

    Update trig-int.ino

--- trig-int/trig-int.ino
+++ trig-int/trig-int.ino
@@ -1,7 +1,14 @@
+#define PIN_TRIG 1
+
 void setup() {
   Serial.begin(115200);
+  attachInterrupt(digitalPinToInterrupt(PIN_TRIG), func, RISING);
+  
 }
 
 void loop() {
+}
 
+void func() {
+  Serial.println("i");
 }
